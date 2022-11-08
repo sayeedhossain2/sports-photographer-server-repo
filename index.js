3920ea051a6ae0a21acc021f2bdb5511fea33279
sayeedhossain2 <sayeedh95@gmail.com>
2022-11-08 17:10:49 +0600

    limit option emplement

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 
 // middle wares
@@ -36,6 +36,13 @@ async function run() {
       const cursor = serviceCollection.find(query);
       const service = await cursor.toArray();
       res.send(service);
+    });
+
+    app.get("/singleService/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const singleService = await serviceCollection.findOne(query);
+      res.send(singleService);
     });
   } finally {
   }
