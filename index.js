b5e11ce6dd2dec8ece028902cb381d288d2cc177
sayeedhossain2 <sayeedh95@gmail.com>
2022-11-10 17:48:13 +0600

    some change

--- index.js
+++ index.js
@@ -11,12 +11,28 @@ app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.khpqtwr.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
+
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
+
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: "unauthorized access" });
+  }
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+    if (err) {
+      return res.status(401).send({ message: "unauthorized access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+}
+
 async function run() {
   try {
     // 1st database and collection
@@ -27,6 +43,15 @@ async function run() {
     const reviewCollection = client
       .db("sportsPhotographer")
       .collection("review");
+
+    // JWT
+    app.post("/jwt", (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1hr",
+      });
+      res.send({ token });
+    });
 
     // post data from site
     app.post("/AddService", async (req, res) => {
@@ -74,7 +99,14 @@ async function run() {
       res.send(allReviewer);
     });
     //  only my reviev (find with email)
-    app.get("/userReview", async (req, res) => {
+    app.get("/userReview", verifyJWT, async (req, res) => {
+      const decoded = req.decoded;
+      console.log(decoded, req.query.email);
+      // console.log("inside api", decoded);
+      if (decoded.email !== req.query.email) {
+        res.status(403).send({ message: "forbiden" });
+      }
+
       let query = {};
       if (req.query.email) {
         query = {
@@ -86,7 +118,7 @@ async function run() {
       res.send(userReview);
     });
 
-    // put method
+    // put get data  method
     app.get("/usersReview/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
@@ -94,6 +126,7 @@ async function run() {
       res.send(user);
     });
 
+    // put update method
     app.put("/usersReview/:id", async (req, res) => {
       const id = req.params.id;
       const filter = { _id: ObjectId(id) };
